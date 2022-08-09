b6aa2753d1f72395ce9682cc0a7182f08fe03cee
abinsinguza-dallen <dallenabinsinguza@gmail.com>
2022-08-09 18:06:25 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var personalInfo=document.getElementById("#contact")
+addElement.appendChild(document.createTextNode("Sky garden"))
+var personalInfo=document.getElementById("#contact").appendChild(addElement)
+var companyname = window.prompt("companyname ");
+alert(companyname);
+
+
+
+var personalInfo=document.getElementById("#contact")
+addElement.appendChild(document.createTextNode("07694994595"))
+var personalInfo=document.getElementById("#contact").appendChild(addElement)
+var phonenumber = window.prompt("phonenumber");
+alert(phonenumber);
+
+
+var personalInfo=document.getElementById("#contact")
+addElement.appendChild(document.createTextNode("Uganda"))
+var personalInfo=document.getElementById("#contact").appendChild(addElement)
+var location = window.prompt("location: ");
+alert( location);
+
+
